52375e07db024255306a20905a78f27162dbacbe
elhoussam <houssam.glsd@gmail.com>
2021-02-05 19:44:46 +0100

    design form for CRUD Alimentation Oprs

---
+++ StartPoint/Alimentation/InsertUpdateAlimentation.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace GGAO.Alimentation
+{
+    partial class InsertUpdateAlimentation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.EngineCombobox = new VMultiColumnComboBox.MultiColumComboBox();
+            this.ProductCombobox = new VMultiColumnComboBox.MultiColumComboBox();
+            this.PoleCombobox = new VMultiColumnComboBox.MultiColumComboBox();
+            this.DriverCombobox = new VMultiColumnComboBox.MultiColumComboBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DriverCombobox);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.PoleCombobox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ProductCombobox);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.EngineCombobox);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(560, 265);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Information";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.SaveButton);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 205);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(560, 60);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Operation";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Reference";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kilométrage";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Date";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Quanité";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(285, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Pole";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(285, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Produit";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(285, 69);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Engine";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(52, 107);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 20);
+            this.label8.TabIndex = 4;
+            this.label8.Text = "Type";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(285, 175);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Chauffeure";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(111, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(436, 26);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(111, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 26);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(111, 175);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(158, 26);
+            this.textBox4.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(111, 101);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(158, 28);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(111, 67);
+            this.dateTimePicker1.MaxDate = new System.DateTime(2200, 2, 5, 0, 0, 0, 0);
+            this.dateTimePicker1.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(158, 26);
+            this.dateTimePicker1.TabIndex = 14;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 2, 5, 0, 0, 0, 0);
+            // 
+            // EngineCombobox
+            // 
+            this.EngineCombobox.ColumnWidth = null;
+            this.EngineCombobox.DataSource = null;
+            this.EngineCombobox.DisplayColumnNo = 1;
+            this.EngineCombobox.DropDownHeight = 200;
+            this.EngineCombobox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.EngineCombobox.GridLines = VMultiColumnComboBox.GridLines.None;
+            this.EngineCombobox.Location = new System.Drawing.Point(372, 65);
+            this.EngineCombobox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.EngineCombobox.Name = "EngineCombobox";
+            this.EngineCombobox.SelectedItem = null;
+            this.EngineCombobox.ShowHeader = false;
+            this.EngineCombobox.Size = new System.Drawing.Size(175, 26);
+            this.EngineCombobox.SourceDataHeader = null;
+            this.EngineCombobox.SourceDataString = null;
+            this.EngineCombobox.TabIndex = 15;
+            this.EngineCombobox.ValueColumnNo = 0;
+            // 
+            // ProductCombobox
+            // 
+            this.ProductCombobox.ColumnWidth = null;
+            this.ProductCombobox.DataSource = null;
+            this.ProductCombobox.DisplayColumnNo = 1;
+            this.ProductCombobox.DropDownHeight = 200;
+            this.ProductCombobox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ProductCombobox.GridLines = VMultiColumnComboBox.GridLines.None;
+            this.ProductCombobox.Location = new System.Drawing.Point(372, 101);
+            this.ProductCombobox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ProductCombobox.Name = "ProductCombobox";
+            this.ProductCombobox.SelectedItem = null;
+            this.ProductCombobox.ShowHeader = false;
+            this.ProductCombobox.Size = new System.Drawing.Size(175, 26);
+            this.ProductCombobox.SourceDataHeader = null;
+            this.ProductCombobox.SourceDataString = null;
+            this.ProductCombobox.TabIndex = 16;
+            this.ProductCombobox.ValueColumnNo = 0;
+            // 
+            // PoleCombobox
+            // 
+            this.PoleCombobox.ColumnWidth = null;
+            this.PoleCombobox.DataSource = null;
+            this.PoleCombobox.DisplayColumnNo = 1;
+            this.PoleCombobox.DropDownHeight = 200;
+            this.PoleCombobox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.PoleCombobox.GridLines = VMultiColumnComboBox.GridLines.None;
+            this.PoleCombobox.Location = new System.Drawing.Point(372, 137);
+            this.PoleCombobox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PoleCombobox.Name = "PoleCombobox";
+            this.PoleCombobox.SelectedItem = null;
+            this.PoleCombobox.ShowHeader = false;
+            this.PoleCombobox.Size = new System.Drawing.Size(175, 26);
+            this.PoleCombobox.SourceDataHeader = null;
+            this.PoleCombobox.SourceDataString = null;
+            this.PoleCombobox.TabIndex = 17;
+            this.PoleCombobox.ValueColumnNo = 0;
+            // 
+            // DriverCombobox
+            // 
+            this.DriverCombobox.ColumnWidth = null;
+            this.DriverCombobox.DataSource = null;
+            this.DriverCombobox.DisplayColumnNo = 1;
+            this.DriverCombobox.DropDownHeight = 200;
+            this.DriverCombobox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.DriverCombobox.GridLines = VMultiColumnComboBox.GridLines.None;
+            this.DriverCombobox.Location = new System.Drawing.Point(372, 172);
+            this.DriverCombobox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.DriverCombobox.Name = "DriverCombobox";
+            this.DriverCombobox.SelectedItem = null;
+            this.DriverCombobox.ShowHeader = false;
+            this.DriverCombobox.Size = new System.Drawing.Size(175, 26);
+            this.DriverCombobox.SourceDataHeader = null;
+            this.DriverCombobox.SourceDataString = null;
+            this.DriverCombobox.TabIndex = 18;
+            this.DriverCombobox.ValueColumnNo = 0;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.SaveButton.Location = new System.Drawing.Point(456, 15);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(91, 33);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // InsertUpdateAlimentation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 265);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "InsertUpdateAlimentation";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private VMultiColumnComboBox.MultiColumComboBox DriverCombobox;
+        private VMultiColumnComboBox.MultiColumComboBox PoleCombobox;
+        private VMultiColumnComboBox.MultiColumComboBox ProductCombobox;
+        private VMultiColumnComboBox.MultiColumComboBox EngineCombobox;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
---
+++ StartPoint/Alimentation/InsertUpdateAlimentation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GGAO.Alimentation
+{
+    public partial class InsertUpdateAlimentation : Form
+    {
+        public InsertUpdateAlimentation()
+        {
+            InitializeComponent();
+        }
+    }
+}
